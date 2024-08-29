1922c40f4dca414447ebacc58d8ec9252615468b
hieupt <hieuphamtrung@admicro.vn>
2024-08-29 16:11:13 +0700

    update

--- index.php
+++ index.php
@@ -29,9 +29,9 @@ define('DB_HOST', '127.0.0.1');
 // 定义数据库名
 define('DB_NAME', 'busnt');
 // 定义数据库账号
-define('DB_USER', 'busnt');
+define('DB_USER', 'root');
 // 定义数据库密码
-define('DB_PWD', 'busnt');
+define('DB_PWD', '');
 // 定义数据库端口
 define('DB_PORT', '3306');
 
