189d2c1902c3a0e11524be9abdd45df1430c6188
PhamHieu <phamhieu078@gmail.com>
2024-09-01 09:14:42 +0700

    update

--- index.php
+++ index.php
@@ -27,11 +27,11 @@ define('DB_TYPE', 'mysql');
 // 定义数据库地址  蜜桃源码网--www.mitaobo.com
 define('DB_HOST', '127.0.0.1');
 // 定义数据库名
-define('DB_NAME', 'busnt');
+define('DB_NAME', 'bunst');
 // 定义数据库账号
 define('DB_USER', 'root');
 // 定义数据库密码
-define('DB_PWD', '');
+define('DB_PWD', 'your_password');
 // 定义数据库端口
 define('DB_PORT', '3306');
 
